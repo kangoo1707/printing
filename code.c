ad907682904b2a02194c4c50b6ed6d147816d59c
kangoo1707 <kangoo1707@gmail.com>
2011-08-12 01:28:34 +0700

    delete all, leave print6

--- code.c
+++ code.c
@@ -1,26 +1,5 @@
 #include <stdio.h>
 
-void print1() {
-	printf("1");
-}
-
-void print2() {
-	printf("2");
-	printf("release1");
-}
-
-void print3() {
-	printf("master");
-}
-
-void print4() {
-	printf("4");
-}
-
-void print5() {
-	printf("tagged 1.0");
-}
-
 void print6() {
 	printf("using a remote repo");
 }
